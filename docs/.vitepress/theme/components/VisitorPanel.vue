184c6ff8bfe847ce844078e6764c034b56a227e7
by297801 <172978010@qq.com>
2025-07-23 16:19:22 +0800

    修改静态资源

--- docs/.vitepress/theme/components/VisitorPanel.vue
+++ docs/.vitepress/theme/components/VisitorPanel.vue
@@ -6,7 +6,7 @@
           本站总访问量
           <span id="busuanzi_value_site_pv" class="font-bold">--</span> 次
         </span>
-        <img src="/heart.gif" alt="heart" class="heart-img" width="50" height="50" @click="onLinkUmiHandle" />
+        <!-- <img src="/heart.gif" alt="heart" class="heart-img" width="50" height="50" @click="onLinkUmiHandle" /> -->
         <span class="text">
           本站访客数
           <span id="busuanzi_value_site_uv" class="font-bold">--</span> 人次
